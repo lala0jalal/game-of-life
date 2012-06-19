fec367b950c1839b97272803588cfae663bbd679
lala0jalal <lala.jalal@gmail.com>
2012-06-19 13:03:59 -0300

    Update master

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.wakaleo.gameoflife.domain.Universe;
 
 @Controller
-@RequestMapping("/game")
+@RequestMapping"/game")
 public class GameController {
 
     Random randomGenerator = new Random();
